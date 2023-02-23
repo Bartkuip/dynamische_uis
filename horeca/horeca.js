f4554cb24439a181023ed5e674c19f4042481315
Bartkuip <31472937+Bartkuip@users.noreply.github.com>
2023-02-23 02:52:03 +0100

    update all

--- horeca/horeca.js
+++ horeca/horeca.js
@@ -1,5 +1,4 @@
 var test = true
-var drank = {}
 var drank = {"bier": {name: "bier", price : 2.30, amount: 0}, "wijn": {name: "wijn", price: 4.50, amount: 0}, "fris": {name: "fris", price: 2.00, amount: 0}}
 //drank.bier.amount += 
 //drank['bier']['amount'] += parseInt()
@@ -9,6 +8,7 @@ while (test) {
     userInput = prompt("Wat wilt u bestellen?").toLowerCase();
     if (userInput in drank){
         orderAmount = parseInt(prompt("Hoeveel " + userInput + " wilt u?"))
+        console.log(userInput)
         drank.userInput.amount += orderAmount
     } else if(userInput == "stop"){
         var test = false;
@@ -16,9 +16,7 @@ while (test) {
         console.log("Dit ken ik niet.");
     }
 }
-console.log("Bonnetje:")
-console.log(drank); 
-/*for (var key in drank) {
-    console.log(drank); 
-
-} */
+console.log("Bonnetje:");
+console.log(drank.bier.amount + "x bier: " + Math.round(drank.bier.amount * drank.bier.price) + " euro."); 
+console.log(drank.wijn.amount + "x wijn: " + Math.round(drank.wijn.amount * drank.wijn.price) + " euro."); 
+console.log(drank.fris.amount + "x fris: " + Math.round(drank.fris.amount * drank.fris.price) + " euro."); 
