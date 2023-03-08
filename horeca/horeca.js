3606ce733e8e2fcdef81693a021908fff0ca17eb
Bartkuip <31472937+Bartkuip@users.noreply.github.com>
2023-03-08 11:36:39 +0100

    horeca klaar

--- horeca/horeca.js
+++ horeca/horeca.js
@@ -5,15 +5,16 @@ while (test) {
     userInput = prompt("Wat wilt u bestellen?").toLowerCase();
     if (userInput in drank){
         orderAmount = parseInt(prompt("Hoeveel " + userInput + " wilt u?"))
-        console.log(userInput)
-        drank.userInput.amount += orderAmount
+        drank[userInput].amount += orderAmount
     } else if(userInput == "stop"){
         var test = false;
     } else {
         console.log("Dit ken ik niet.");
     }
 }
+totaal = Math.round(drank.bier.price * drank.bier.amount + drank.wijn.price * drank.wijn.amount + drank.fris.price * drank.fris.amount)
 console.log("Bonnetje:");
 console.log(drank.bier.amount + "x bier: " + Math.round(drank.bier.amount * drank.bier.price) + " euro."); 
 console.log(drank.wijn.amount + "x wijn: " + Math.round(drank.wijn.amount * drank.wijn.price) + " euro."); 
-console.log(drank.fris.amount + "x fris: " + Math.round(drank.fris.amount * drank.fris.price) + " euro."); 
+console.log(drank.fris.amount + "x fris: " + Math.round(drank.fris.amount * drank.fris.price) + " euro.");
+console.log("Totaal prijs is: " + totaal + " euro.") 
