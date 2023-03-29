6cd4fd43c629f3f2c7ebb9a916be622fa94e8c18
Bartkuip <99027534@mydavinci.nl>
2023-03-29 11:27:31 +0200

    planning 29 maart

--- horeca/horeca.js
+++ horeca/horeca.js
@@ -1,20 +1,33 @@
-var test = true
-var drank = {"bier": {name: "bier", price : 2.30, amount: 0}, "wijn": {name: "wijn", price: 4.50, amount: 0}, "fris": {name: "fris", price: 2.00, amount: 0}}
+active = true
+var drank = {"bier": {name: "bier", price : 2, amount: 0}, "wijn": {name: "wijn", price: 4, amount: 0}, "fris": {name: "fris", price: 3, amount: 0}}
 
-while (test) {
+while (active) {
     userInput = prompt("Wat wilt u bestellen?").toLowerCase();
-    if (userInput in drank){
-        orderAmount = parseInt(prompt("Hoeveel " + userInput + " wilt u?"))
-        drank[userInput].amount += orderAmount
-    } else if(userInput == "stop"){
-        var test = false;
+    drankSelect(userInput)
+}
+function drankSelect(drankInput){
+    if (drankInput in drank){
+        orderAmount = parseInt(prompt("Hoeveel " + drankInput + " wilt u?"))
+        drank[drankInput].amount += orderAmount
+    } else if(drankInput == "stop"){
+        drankReceipt()
+        active = false;
     } else {
         console.log("Dit hebben wij niet..");
-    }
+    }   
 }
-totaal = drank.bier.price * drank.bier.amount + drank.wijn.price * drank.wijn.amount + drank.fris.price * drank.fris.amount.toFixed(2)
-console.log("Bonnetje:");
-console.log(drank.bier.amount + "x bier: " + drank.bier.amount * drank.bier.price + " euro."); 
-console.log(drank.wijn.amount + "x wijn: " + drank.wijn.amount * drank.wijn.price + " euro."); 
-console.log(drank.fris.amount + "x fris: " + drank.fris.amount * drank.fris.price + " euro.");
-console.log("Totaal prijs is: " + totaal.toFixed(2) + " euro.")
+
+function drankReceipt(){
+    totaal = drank.bier.price * drank.bier.amount + drank.wijn.price * drank.wijn.amount + drank.fris.price * drank.fris.amount
+    console.log("Bonnetje:");
+    if (drank.bier.amount > 0){
+    console.log(drank.bier.amount + "x bier: " + drank.bier.amount * drank.bier.price + " euro.");
+    }
+    if(drank.wijn.amount > 0){
+    console.log(drank.wijn.amount + "x wijn: " + drank.wijn.amount * drank.wijn.price + " euro.");
+    }
+    if(drank.fris.amount > 0){
+    console.log(drank.fris.amount + "x fris: " + drank.fris.amount * drank.fris.price + " euro.");
+    }
+    console.log("Totaal prijs is: " + totaal.toFixed(2) + " euro.")
+}
